f0377b5625f52f8e5250639dfbdde2a2615c0ef4
Capsiz-Artist <157418701+Capsiz-Artist@users.noreply.github.com>
2025-04-03 15:16:25 +0300

    Update projects.js
    10 değişken global olarak tanımlandı

--- İnsiderfinal/projects/projects.js
+++ İnsiderfinal/projects/projects.js
@@ -18,6 +18,16 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
+let monsterName;
+let monsterHealthText;
+let monsterStats;
+let xpText;
+let healthText;
+let text;
+let goldText;
+let button1;
+let button2;
+let button3;
 
 const weapons = [
     { name: 'stick', power: 5 },
@@ -288,16 +298,16 @@ function pick(guess) {
 }
 
 function initGame() {
-    const button1 = document.querySelector('#button1');
-    const button2 = document.querySelector("#button2");
-    const button3 = document.querySelector("#button3");
-    const text = document.querySelector("#text");
-    const xpText = document.querySelector("#xpText");
-    const healthText = document.querySelector("#healthText");
-    const goldText = document.querySelector("#goldText");
-    const monsterStats = document.querySelector("#monsterStats");
-    const monsterName = document.querySelector("#monsterName");
-    const monsterHealthText = document.querySelector("#monsterHealth");
+    button1 = document.querySelector('#button1');
+    button2 = document.querySelector("#button2");
+    button3 = document.querySelector("#button3");
+    text = document.querySelector("#text");
+    xpText = document.querySelector("#xpText");
+    healthText = document.querySelector("#healthText");
+    goldText = document.querySelector("#goldText");
+    monsterStats = document.querySelector("#monsterStats");
+    monsterName = document.querySelector("#monsterName");
+    monsterHealthText = document.querySelector("#monsterHealth");
 
     button1.onclick = goStore;
     button2.onclick = goCave;
